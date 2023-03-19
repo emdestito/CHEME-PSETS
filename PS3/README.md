c92e94275eecd39829e79fa7fe03bac045709a3e
emdestito <113158893+emdestito@users.noreply.github.com>
2023-03-18 20:39:26 -0400

    ps3 edits

---
+++ PS3/README.md
@@ -0,0 +1,13 @@
+
+Collaborators: Emily, Liam, and Anthony
+    *We all disucssed and worked through all parts of the problem set together, and Emily typed up our ideas. We also discussed certain aspects with other groups when we got stuck, such as for editing the solvers file where we consulted with Jake.
+
+
+|      | Case 1 |Case 2  | Case 3|
+|------|--------|--------|-------|
+|det(A)|  1 | 1 | 1 |
+|diagonally dominant?| yes | yes | no |
+|Jacobi solution?| yes | yes| yes |
+|Gauss-Seidel solution?| yes | yes | yes|
+|Jacobi error| 2.8 x 10^41 | 0 | 0 |
+|Gauss-Seidel error| 2.8 x 10^41 | 0 | 0 |
--- PS3/Submit.jl
+++ PS3/Submit.jl
@@ -6,24 +6,27 @@ case_1 = build(MyChemicalDecayModel, κ = 1.0, h = 0.1, N = number_of_steps, C�
 case_2 = build(MyChemicalDecayModel, κ = 10.0, h = 0.1, N = number_of_steps, Cₒ = 10.0)
 case_3 = build(MyChemicalDecayModel, κ = 100.0, h = 0.1, N = number_of_steps, Cₒ = 10.0)
 
-
-
-zₒ = zeros(Float64, number_of_steps)
+z = zeros(Float64, number_of_steps)
 
 #For case_1
-jacobi_1 = solve(JacobiIterationSolver(), case_1, zₒ)
-gauss_1 = solve(GaussSeidelIterationSolver(), case_1, zₒ)
-
+jacobi_1 = solve(JacobiIterationSolver(), case_1, z)
+gauss_1 = solve(GaussSeidelIterationSolver(), case_1, z)
 
 #For case_2
-jacobi_2 = solve(JacobiIterationSolver(), case_2, zₒ)
-gauss_2 = solve(GaussSeidelIterationSolver(), case_2, zₒ)
+jacobi_2 = solve(JacobiIterationSolver(), case_2, z)
+gauss_2 = solve(GaussSeidelIterationSolver(), case_2, z)
 
 #For case_3
-jacobi_3 = solve(JacobiIterationSolver(), case_3, zₒ)
-gauss_3 = solve(GaussSeidelIterationSolver(), case_3, zₒ)
+jacobi_3 = solve(JacobiIterationSolver(), case_3, z)
+gauss_3 = solve(GaussSeidelIterationSolver(), case_3, z)
+
+#Calculate the determinants
+det_case_1 = det(case_1.A)
+det_case_2 = det(case_2.A)
+det_case_3 = det(case_3.A)
 
 
+#Calculate inverses
 inv_1 = inv(case_1.A)
 inv_2 = inv(case_2.A)
 inv_3 = inv(case_3.A)
@@ -33,11 +36,6 @@ prod_inv_1 = inv_1*case_1.b
 prod_inv_2 = inv_2*case_2.b
 prod_inv_3 = inv_3*case_3.b
 
-#Calculate the determinants
-det_case_1 = det(case_1.A)
-det_case_2 = det(case_2.A)
-det_case_3 = det(case_3.A)
-
 #Calculate the errors
 j_error_1 = norm(jacobi_1 - prod_inv_1)
 j_error_2 = norm(jacobi_2 - prod_inv_2)
@@ -46,9 +44,3 @@ j_error_3 = norm(jacobi_3 - prod_inv_3)
 g_error_1 = norm(gauss_1 - prod_inv_1)
 g_error_2 = norm(gauss_2 - prod_inv_2)
 g_error_3 = norm(gauss_3 - prod_inv_3)
-
-
-#Check Diagonally Dominant for each case
-println("case_1: ", all(sum(abs.(case_1.A),dims=2) .<= 2abs.(diag(case_1.A))))
-println("case_2: ", all(sum(abs.(case_2.A),dims=2) .<= 2abs.(diag(case_2.A))))
-println("case_3: ", all(sum(abs.(case_3.A),dims=2) .<= 2abs.(diag(case_3.A))))
